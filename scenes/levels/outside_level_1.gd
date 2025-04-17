ca3d76f1b04eca81abb01ea3cba7b19630981ff9
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2025-04-17 14:52:13 +0200

    Level transition

--- scenes/levels/outside_level_1.gd
+++ scenes/levels/outside_level_1.gd
@@ -1,5 +1,11 @@
 extends ParentLevel
 
+
 func _on_gate_on_player_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
+	call_deferred("_change_scene")
+	
+func _change_scene() -> void:
+	var inside: PackedScene = load("res://scenes/levels/inside_level_1.tscn")
+	get_tree().change_scene_to_packed(inside)
