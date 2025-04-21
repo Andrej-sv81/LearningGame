44033b60935c673fe131723a98241681ffc8ff3a
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2025-04-21 11:19:50 +0200

    Ui level transition

--- scenes/interface/ui.gd
+++ scenes/interface/ui.gd
@@ -2,6 +2,11 @@ extends CanvasLayer
 
 @onready var laser_label: Label = $LaserCounter/VBoxContainer/Label
 @onready var grenade_label: Label = $GrenadeCounter/VBoxContainer/Label
+@onready var laser_icon: TextureRect = $LaserCounter/VBoxContainer/TextureRect
+@onready var grenade_icon: TextureRect = $GrenadeCounter/VBoxContainer/TextureRect
+
+var green: Color = Color("6bbfa3")
+var red: Color = Color("ef411b")
 
 func _ready() -> void:
 	update_laser_text()
@@ -9,6 +14,16 @@ func _ready() -> void:
 	
 func update_laser_text() -> void:
 	laser_label.text = str(Globals.laser_amount)
+	update_color(Globals.laser_amount, laser_label, laser_icon)
 	
 func update_grenade_text() -> void:
 	grenade_label.text = str(Globals.grenade_amount)
+	update_color(Globals.grenade_amount, grenade_label, grenade_icon)
+	
+func update_color(amount: int, label: Label, icon: TextureRect) -> void:
+	if amount == 0:
+		label.modulate = red
+		icon.modulate = red
+	else:
+		label.modulate = green
+		icon.modulate = green
