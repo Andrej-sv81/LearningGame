c889976c7135e39e689baa795937e3c017b83ed6
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2025-04-29 12:37:15 +0200

    fade to black level transition

--- scenes/levels/outside_level_1.gd
+++ scenes/levels/outside_level_1.gd
@@ -4,7 +4,7 @@ extends ParentLevel
 func _on_gate_on_player_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
-	call_deferred("_change_scene")
+	TransitionLayer.change_scene("res://scenes/levels/inside_level_1.tscn")
 	
 func _change_scene() -> void:
 	var inside: PackedScene = load("res://scenes/levels/inside_level_1.tscn")
